c23fd0d8b9e99734ee1bd87cca1803ed9db6c93a
Jeongmin Jang <xvsgxv@gmail.com>
2022-03-24 19:08:52 +0900

    Baekjoon
    1072번 : 게임
    1094번 : 막대기

--- Baekjoon/src/Silver/Main1072.java
+++ Baekjoon/src/Silver/Main1072.java
@@ -2,9 +2,8 @@ package Silver;
 
 import java.util.Scanner;
 
-// 시간초과
 public class Main1072 {
-
+	/*// 시간초과
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int X = sc.nextInt();
@@ -22,6 +21,33 @@ public class Main1072 {
 			Y++;
 		}
 		System.out.println(count);
+	}*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long X = sc.nextLong();
+		long Y = sc.nextLong();
+		long Z = (Y*100)/X;
+		// Z가 99보다 크거나 같으면 아무리 횟수를 많이해도 100프로 그 이상이 될 수 없으므로 -1을 출력
+		if(Z>=99) {
+			System.out.println(-1);
+		} else {
+			// Y값의 범위가 0부터 X까지 이므로 시작값음 0 끝값을 1000000000로 둔다.
+			long start = 0;
+			long mid = 0;
+			long end = 1000000000;
+			// 시작점이 끝값보다 큰 지점까지만 계산수행
+			while(start <= end) {
+				// 중간값을 시작과 끝값의 중간으로 잡는다.
+				mid = (start+end)/2;
+				if(Z < 100*(Y+mid)/(X+mid)) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			}
+			// 최소값을 출력해야 하므로 start를 출력한다.
+			System.out.println(start);
+		}
 	}
 
 }
--- Baekjoon/src/Silver/Main1094.java
+++ Baekjoon/src/Silver/Main1094.java
@@ -9,6 +9,8 @@ public class Main1094 {
 		int N = sc.nextInt();
 		int count = 0;
 		while(N > 0) {
+			// 막대기의 길이를 2로 나누었을 때 나머지가 존재한다면 결국 자르다 보면 맨 마지막에 나머지가 없을 때보다 1개의 막대기가 더 필요하므로
+			// 나머지가 1일 때는 count를 증가시킨다.
 			if(N%2 == 1) {
 				count++;
 			} 
