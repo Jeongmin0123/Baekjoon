fc4d6b2734bf1717969fcad45bf82ba0622e5da6
Jeongmin Jang <xvsgxv@gmail.com>
2022-04-08 20:55:33 +0900

    Baekjoon
    1476번 : 날짜 계산

--- Baekjoon/src/Gold/Main1476.java
+++ Baekjoon/src/Gold/Main1476.java
@@ -12,8 +12,10 @@ public class Main1476 {
 		int result = chinese(E, S, M);
 		System.out.println(result);
 	}
+	// 중국인의 나머지 정리를 사용한다
+	// 15, 28, 19가 서로 서로소이기 때문에
 	public static int chinese(int E, int S, int M) {
-		int result = E*28*19 + S*15*19 + M*15*28;
+		int result = E*28*19*13 + S*15*19*17 + M*15*28*10;
 		while(result > 15*28*19) {
 			result -= 15*28*19;
 		}
---
+++ Baekjoon/src/Gold/Main17425.java
@@ -0,0 +1,16 @@
+package Gold;
+
+import java.util.Scanner;
+
+public class Main17425 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int[] test = new int[T];
+		for(int i = 0 ; i < T ; i++) {
+			test[i] = sc.nextInt();
+		}
+	}
+
+}
