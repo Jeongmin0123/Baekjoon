adbe970aacfc76e8d7fd4e78f8f002d3001ab903
Jeongmin Jang <xvsgxv@gmail.com>
2022-04-23 02:20:10 +0900

    Baekjoon
    1520번 : 내리막길

--- Baekjoon/src/Gold/Main1520.java
+++ Baekjoon/src/Gold/Main1520.java
@@ -1,36 +1,62 @@
 package Gold;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main1520 {
-	public static boolean used[][];
+	public static int[][] count;
 	public static int[][] arr;
-	public static int count = 0;
-	public static int[] rangeX = { 0, 0,-1, 1};
-	public static int[] rangeY = {-1, 1, 0, 0};
+	public static int M;
+	public static int N;
+	public static int[] rangeX = {1, 0,-1, 0};
+	public static int[] rangeY = {0, 1, 0,-1};
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		arr = new int[N][M];
-		used = new boolean[N][M];
-		for(int i = 0 ; i < N ; i++) {
-			for(int j = 0 ; j < M ; j++) {
+		M = sc.nextInt();
+		N = sc.nextInt();
+		arr = new int[M+1][N+1];
+		count = new int[M+1][N+1];
+		for(int i = 1 ; i <= M ; i++) {
+			for(int j = 1 ; j <= N ; j++) {
 				arr[i][j] = sc.nextInt();
+				count[i][j] = -1;
 			}
 		}
+		int result = check(1,1);
+		System.out.println(result);
 		
 		
 	}
 	
 	public static int check(int x, int y) {
-		if(x == arr.length && y == arr[0].length) {
+		// 원하는 지점에 도착시에 1을 반환하고 종료한다.
+		// 추가로 다시 움직일 필요가 없기 때문이다.
+		if(x == M && y == N) {
 			return 1;
-		} else if(x == 0) {
-			
+		}
+		// -1이 아닌 경우 이미 방문한 경우이므로 전에 방문했을 때의
+		// 값을 반환한다.
+		if(count[x][y] != -1) {
+			return count[x][y];
+		} else {
+			count[x][y] = 0;
+			// 상하좌우 움직인다.
+			for(int i = 0 ; i < 4 ; i++) {
+				int dx = x + rangeX[i];
+				int dy = y + rangeY[i];
+				// 움직일 수 없는 구간일 경우 넘어간다.
+				if(dx < 1 || dy < 1 || dx > M || dy > N) {
+					continue;
+				}
+				// 만약 움직인 부분의 숫자가 더 작다면,
+				// 경로의 수에 추가한다.
+				if(arr[x][y] > arr[dx][dy]) {
+					count[x][y] += check(dx,dy);
+				}
+			}
 		}
 		
-		return 1;
+		return count[x][y];
 	}
 
 }
