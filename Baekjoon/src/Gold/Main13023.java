cc2debb0f772e42c4376d9238c2004eeec40e763
장정민 <xvsgxv@gmail.com>
2022-10-21 00:27:32 +0900

    Baekjoon 13023번 : ABCDE

--- Baekjoon/src/Gold/Main13023.java
+++ Baekjoon/src/Gold/Main13023.java
@@ -8,11 +8,12 @@ import java.util.StringTokenizer;
 
 public class Main13023 {
 
-	static class Node {
+	// 시작점과 끝점에 대한 정보, 즉 친구 관계를 저장하는 클래스
+	static class Edge {
 		int to;
-		Node next;
+		Edge next;
 
-		public Node(int to, Node next) {
+		public Edge(int to, Edge next) {
 			super();
 			this.to = to;
 			this.next = next;
@@ -21,7 +22,7 @@ public class Main13023 {
 	}
 
 	public static int N, M;
-	public static Node[] list;
+	public static Edge[] list;
 	public static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
@@ -29,16 +30,17 @@ public class Main13023 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		// node들의 집합
-		list = new Node[N];
+		// Edge들의 집합
+		list = new Edge[N];
 		visited = new boolean[N];
 		for (int i = 0; i < M; i++) {
+			// 두 사람이 친구관계인 경우 연결해준다
 			st = new StringTokenizer(br.readLine(), " ");
 			int n1 = Integer.parseInt(st.nextToken());
 			int n2 = Integer.parseInt(st.nextToken());
-			// 입력받은 node들을 연결해준다.
-			list[n1] = new Node(n2, list[n1]);
-			list[n2] = new Node(n1, list[n2]);
+			// 입력받은 Edge들을 연결해준다.
+			list[n1] = new Edge(n2, list[n1]);
+			list[n2] = new Edge(n1, list[n2]);
 		}
 		// 모든 노드에 대하여 depth를 조사한다
 		for(int i = 0 ; i < N ; i++) {
@@ -47,7 +49,7 @@ public class Main13023 {
 		System.out.println(0);
 	}
 
-	// dfs 알고리즘을 이용하여 시작점에서 depth가 5가 존재하는 경우 1을 출력한 뒤 종료하는 메서드
+	// dfs 알고리즘을 이용하여 시작점에서 depth가 5가 존재하는 경우가 A는 E와 연결된 경우이므로 그 즉시 1을 출력하고 연산을 종료한다.
 	public static void dfs(int cur, int depth) {
 		visited[cur] = true;
 		if (depth == 5) {
@@ -55,9 +57,9 @@ public class Main13023 {
 			System.exit(0);
 		}
 
-		for (Node node = list[cur]; node != null; node = node.next) {
-			if(!visited[node.to]) {
-				dfs(node.to, depth+1);
+		for (Edge Edge = list[cur]; Edge != null; Edge = Edge.next) {
+			if(!visited[Edge.to]) {
+				dfs(Edge.to, depth+1);
 			}
 		}
 		
